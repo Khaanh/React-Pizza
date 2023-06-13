1d59673e41db2d6696b5499978627336b996c036
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-06-14 00:09:26 +0200

    Created page: Not-found & added React-route

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./scss/app.scss";
+
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const urlPizza = "https://6487922abeba62972790d1b4.mockapi.io/Items";
-
-  useEffect(() => {
-    fetch(urlPizza)
-      .then((response) => response.json())
-      .then((data) => setItems(data));
-  }, [urlPizza]);
-
   return (
     <div className="wrapper">
       <Header />
-
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIstLoading] = useState(true);
+  const urlPizza = "https://6487922abeba62972790d1b4.mockapi.io/Items";
+
+  useEffect(() => {
+    fetch(urlPizza)
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data);
+        setIstLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : [items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)]}
+      </div>
+    </>
+  )
+}
+
+export default Home;
