9518f5069b61febc9f638320a651f931cd4616e2
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-06-18 12:16:45 +0200

    Search function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIstLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
@@ -22,8 +22,9 @@ const Home = () => {
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `search=${searchValue}` : ''
 
-    fetch(`https://6487922abeba62972790d1b4.mockapi.io/Items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://6487922abeba62972790d1b4.mockapi.io/Items?${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then((response) => response.json())
       .then((data) => {
         setItems(data);
@@ -31,7 +32,19 @@ const Home = () => {
       });
 
     window.scrollTo(0, 0)
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
+
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  // .filter(obj => {
+  //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+  //     return true
+  //   } return false
+  // }).filter(obj => {
+  //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+  //     return true
+  //   } return false
+  // })
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className="container">
@@ -41,9 +54,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : [items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)]}
+        {isLoading ? skeletons : pizzas}
       </div>
     </div>
   )
