3fe2feec1832bd6212eb7e4f4bc451f037b69355
Khanh Nguyen <Khanh13Nguyen13@gmail.com>
2023-06-21 01:05:46 +0200

    Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { createContext, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlice";
 import "./scss/app.scss";
-
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -11,11 +12,26 @@ export const SearchContext = createContext("");
 
 function App() {
   const [searchValue, setSearchValue] = useState("");
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   console.log("searchValue", searchValue);
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      {/* <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <Routes>
@@ -24,7 +40,7 @@ function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
